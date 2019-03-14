e95dcb3e36bdb23d855c430fcaf93082a031c174
DAT-Asset-164 <>
2019-03-14 18:57:52 +0530

    Added list ingredient feature

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # recipe-app-api
-![image](https://travis-ci.org/samims/recipe-app-api.svg?branch=master)
+[![image](https://travis-ci.org/samims/recipe-app-api.svg?branch=master)]('https://travis-ci.org/samims/recipe-app-api')
 
 Recipe app api source code(Test driven development)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -43,9 +43,19 @@ class ModelTests(TestCase):
 
     def test_tag_str(self):
         """Test the tag string representation"""
+
         tag = models.Tag.objects.create(
             user=sample_user(),
             name='Vegan'
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
