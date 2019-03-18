6d43bdb00a7d3933afc32ee8c49c1e854219ee95
DAT-Asset-164 <>
2019-03-18 20:02:34 +0530

    Added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -45,3 +45,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         )
 
         read_only_fields = ("id",)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a Recipe Detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
