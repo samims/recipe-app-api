4ab8af1b8aa2f3ce23eaffca5f588594e9f08264
Samiul Sk <>
2019-05-08 16:34:48 +0530

    Added image field to recipe model

--- app/app/urls.py
+++ app/app/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('api/recipe/', include('recipe.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns +=static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
